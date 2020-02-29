0f0dc30b17ba59d55f6cafc322608ec5e85f951d
Loïc Hamet <loic.hamet@gmail.com>
2020-02-29 19:44:08 +0100

    Note component

--- src/Cell.svelte
+++ src/Cell.svelte
@@ -1,23 +1,47 @@
 <script>
+import Note from "./Note.svelte";
+
 export let inFirstString = false;
+export let on = true;
 </script>
 
 <div
   class="cell"
   class:in-first-string={inFirstString}
-/>
+>
+  {#if on}
+    <div class="note">
+      <Note />
+    </div>
+  {/if}
+</div>
 
 <style>
 .cell {
+  position: relative;
   flex-grow: 1;
   padding-top: var(--cell-ratio);
-}
-
-.cell:not(.in-first-string) {
   border-left: var(--black) solid 3px;
 }
 
+.cell.in-first-string {
+  border-color: transparent;
+}
+
+.cell:last-child {
+  border-right: transparent solid 3px;
+}
+
 .cell:not(.in-first-string):last-child {
-  border-right: var(--black) solid 3px;
+  border-color: var(--black);
+}
+
+.note {
+  position: absolute;
+  top: 50%;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ src/Note.svelte
@@ -0,0 +1,11 @@
+<div class="note" />
+
+<style>
+.note {
+  --size: 30%;
+  border-radius: 50%;
+  background-color: var(--black);
+  width: var(--size);
+  padding-top: var(--size);
+}
+</style>
